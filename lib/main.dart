293f0c5c8d6585435ee19281c63060bcebe71666
Marco Leonardini <marco_leonardini@hotmail.com>
2021-11-29 12:54:49 -0400

    release

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Fluqweqweqweros'),
+      home: const MyHomePage(title: 'Flutter reealse'),
     );
   }
 }
